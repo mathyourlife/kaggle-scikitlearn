c960d73301603ae9f7d4fa656c42ba27aea9befd
Gordon Davenport <g.m.davenport@gmail.com>
2013-04-06 22:47:58 -0400

    More play stuffs

--- submissions/05-kneighborsclassifier.py
+++ submissions/05-kneighborsclassifier.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+from sklearn.svm import NuSVR
+from sklearn.svm import LinearSVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree  import DecisionTreeClassifier
+
 def write_out(data):
     f = open('testLabels.csv', 'wb')
     for val in data:
@@ -9,59 +14,65 @@ train = np.genfromtxt('../data/train.csv', delimiter=',')
 trainLabels = np.genfromtxt('../data/trainLabels.csv', delimiter=',', dtype=int)
 test = np.genfromtxt('../data/test.csv', delimiter=',')
 
-from sklearn.naive_bayes import GaussianNB
-
-clf = GaussianNB()
-
-clf = clf.fit(train, trainLabels)
-
-validate = clf.predict(train)
-
-print sum(abs(validate - trainLabels))
-
-testLabels = clf.predict(test)
-
-from sklearn.svm import LinearSVC
-
-clf2 = LinearSVC(loss = 'l2')
+clf2 = DecisionTreeClassifier()
 
 clf2 = clf2.fit(train, trainLabels)
 
 validate2 = clf2.predict(train)
 
+print validate2
+
 print sum(abs(validate2 - trainLabels))
 
-testLabels = clf2.predict(test)
+#clf2 = LinearSVC(loss = 'l2')
 
-from sklearn.neighbors import  KNeighborsClassifier
+#clf2 = clf2.fit(train[100:200], trainLabels[100:200])
 
-clf3 = KNeighborsClassifier()
+#validate3 = clf2.predict(train[100:200])
 
-clf3 = clf3.fit(train, trainLabels)
+#validate4 = np.concatenate((validate2,validate3),axis=0)
 
-validate3 = clf3.predict(train)
+#print validate4
 
-print validate3
+#print sum(abs(validate4 - trainLabels[0:200]))
 
-print sum(abs(validate3 - trainLabels))
-
-testLabels = clf3.predict(test)
-
-write_out (testLabels)
+i = 0;
+c = 50;
 
 
-retry =  train[validate3 != trainLabels]
-retrylabel =  trainLabels[validate3 != trainLabels]
+#while c < 1000:
+##validate = np.array([])
+##i = c;
+##print len(train),
+##clf2 = LinearSVC(loss = 'l2', fit_intercept = 0)
+###clf2 = KNeighborsClassifier()
+##for i in range(0,len(train),50):
+##    print i
+##   
+##    #if i == 0:
+##    clf2 = clf2.fit(train[i:i+c], trainLabels[i:i+c])
+##
+##    validate2 = clf2.predict(train[i:i + c])
+##
+##    validate = np.concatenate((validate,validate2),axis=0)
+##
+##    print len(validate)
+##
+##       
+##    
+##           
+##print sum(abs(validate - trainLabels)) 
+##    #c += 100;
+    
+        
+    
+    
 
-print retry
 
-clf2 = LinearSVC(loss = 'l2')
 
-clf2 = clf2.fit(retry, retrylabel)
 
-validate2 = clf2.predict(retry)
 
-print sum(abs(validate2 - retrylabel))
+
 
 
 
